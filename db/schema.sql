fa2bbaef04c49446f4c9c915c78dfbfc6f90a703
Rocket Appliances <monaco.vin@gmail.com>
2022-08-19 14:32:08 -0500

    ADDED not null to everything but manager_id

--- db/schema.sql
+++ db/schema.sql
@@ -12,16 +12,16 @@ CREATE TABLE department(
 /* Create ROLE */
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(35) NULL,
-    salary DECIMAL(10.3) NULL,
-    department_id INT NULL,
+    title VARCHAR(35) NOT NULL,
+    salary DECIMAL(10.3) NOT NULL,
+    department_id INT NOT NULL,
 )
 
 /* Create EMPLOYEE */
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(35) NULL,
-    last_name VARCHAR(35) NULL,
-    role_id INT NULL,
+    first_name VARCHAR(35) NOT NULL,
+    last_name VARCHAR(35) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT NULL,
 )
