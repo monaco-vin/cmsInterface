0e47a23ef17c8849bbf4e43c2e1c439a72220c7c
Rocket Appliances <monaco.vin@gmail.com>
2022-08-19 14:34:26 -0500

    ADDED schema varchar changed to 30 and REMOVED 10.3 from decimal

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,22 @@ USE employee_db;
 /* Create DEPARTMENT */
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    employee_name VARCHAR(35) NOT NULL
+    employee_name VARCHAR(30) NOT NULL
 );
 
 /* Create ROLE */
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(35) NOT NULL,
-    salary DECIMAL(10.3) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
 )
 
 /* Create EMPLOYEE */
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(35) NOT NULL,
-    last_name VARCHAR(35) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
 )
